ed23fd31b41471e2ff82e5e1415fe9dab42d2a73
Mo3et <monet_lee@163.com>
2020-02-19 00:11:18 +0800

    19

--- LotteryDrawing.java
+++ LotteryDrawing.java
@@ -3,21 +3,23 @@ import java.util.*;
 /**
  * LotteryDrawing
  */
-/* 彩票系统 */
+/*
+ * * 彩票系统   出自Java核心技术 卷I P83~84 
+ */
 public class LotteryDrawing {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        //需要彩票的张数
+        // 需要彩票的张数
         System.out.println("How many numbers do you need to draw?");
         int k = in.nextInt();
-        //彩票允许的最大值是多少
+        // 彩票允许的最大值是多少
         System.out.println("What is the highest number you can draw?");
         int n = in.nextInt();
         in.close();
         // fill an array with numbers 1 2 3 ..n
         int[] numbers = new int[n];// fill an .array ..n
         for (int i = 0; i < numbers.length; i++)
-            numbers[i] = i + 1;//表示between 1 and n
+            numbers[i] = i + 1;// 表示between 1 and n
         // draw k numbers and put them into a second array
         int[] result = new int[k];
         for (int i = 0; i < result.length; i++) {
