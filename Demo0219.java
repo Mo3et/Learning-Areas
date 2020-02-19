45cbbe479d556fe9dc06901410ce649fd8bf9032
Mo3et <monet_lee@163.com>
2020-02-19 16:26:24 +0800

    12 upd

---
+++ Demo0219.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+
+/**
+ * Demo0219
+ */
+public class Demo0219 {
+
+    
+    public static void main(final String[] args) {
+        LocalDate ld= LocalDate.now();
+        System.out.println(ld);
+    }
+}
