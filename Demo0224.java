1575dd06d8ccc118f6f0d2a61e964d81a15dcacc
Mo3et <monet_lee@163.com>
2020-02-24 21:24:43 +0800

    0224update

---
+++ Demo0224.java
@@ -0,0 +1,13 @@
+/**
+ * Demo0224
+ */
+// "editor.tokenColorCustomizations": {
+//         "comments": "#0de9d6e8", // 注释  
+// }
+public class Demo0224 {
+
+    public static void main(String[] args) {
+        
+    }
+}
+
