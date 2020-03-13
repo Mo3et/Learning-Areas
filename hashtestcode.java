aadd2ac8760c22adff39b13676054f580418cfda
Mo3et <monet_lee@163.com>
2020-03-14 03:54:16 +0800

    hashtestcde

--- hashtestcode.java
+++ hashtestcode.java
@@ -8,12 +8,17 @@ public class hashtestcode {
     // }
 
     public static void main(String[] args) {
-        String s = "Hello";
+        String s = "OK";
         StringBuilder sb = new StringBuilder(s);
         System.out.println(s.hashCode() + " " + sb.hashCode());
         String t = new String("OK");
         StringBuilder tb = new StringBuilder(t);
         System.out.println(t.hashCode() + " " + tb.hashCode());
+
+        String d = "OK";
+        StringBuilder db = new StringBuilder(d);
+        System.out.println(d.equals(s));
+        System.out.println(sb.equals(db));
     }
 
 }
