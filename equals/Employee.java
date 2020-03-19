50be3987237e94a334ffb7a44b4d8fc9c7b6082f
Mo3et <monet_lee@163.com>
2020-03-19 15:06:30 +0800

    Add to Equals Test

--- equals/Employee.java
+++ equals/Employee.java
@@ -1,6 +1,8 @@
 package equals;
 
 import java.time.LocalDate;
+import java.util.Objects;
+
 
 /**
  * Employee
@@ -23,5 +25,33 @@ public class Employee {
         }
          public LocalDate getHireDay(){
              return hireDay;
+         }
+         public void raiseSalary(double byPercent){
+             double raise=salary*byPercent/100;
+             salary+=raise;
          }   
+         public boolean equals(Object otherObject){
+            //a quick test to see if the objects are identical
+            if (this==otherObject) return true;
+
+            //must return false if the explicit parameter is null
+            if(otherObject==null) return false;
+
+            //if the classes don't match,they can't be equal
+            if(getClass()!=otherObject.getClass())return false;
+
+            //now we know otherObject is a non-null Employee
+            Employee other=(Employee) otherObject;
+
+            //test whether the fields have identical values
+            return Objects.equals(name, other.name)&&salary==other.salary
+            &&Objects.equals(hireDay, other.hireDay);
+         }
+         public int hashCode(){
+             return Objects.hash(name,salary,hireDay);
+         }
+         public String toString(){
+             return getClass().getName()+"[name="+name+",salary="+salary+",hireDay"+hireDay
+             +"]";
+         }
 }
