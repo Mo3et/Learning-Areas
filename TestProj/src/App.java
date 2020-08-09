8118a75a37b87d55a354a0e3fe6bbd2ec81dadca
Mo3et <monet_lee@163.com>
2020-08-09 15:31:43 +0800

    upd Lombok

---
+++ TestProj/src/App.java
@@ -0,0 +1,16 @@
+package TestProj.src;
+
+import java.util.logging.Logger;
+
+import lombok.*;
+import lombok.extern.java.Log;
+@Data
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        String a="bell";
+        Logger.getLogger(a);
+        System.out.println(a);
+    }
+}
